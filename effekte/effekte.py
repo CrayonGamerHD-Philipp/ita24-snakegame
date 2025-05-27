5a4664f3994af85c5ef08899f6b5d24bc3f79711
khaled42 <khalednabea2@gmail.com>
2025-05-27 16:03:13 +0200

    Update Snakegame

--- effekte/effekte.py
+++ effekte/effekte.py
@@ -5,3 +5,4 @@ pygame.mixer.init()
 eat_sound = pygame.mixer.Sound("Sounds/eat.wav")
 background_music = pygame.mixer.Sound("Sounds/background_music.wav")
 game_over_sound = pygame.mixer.Sound("Sounds/game_over.wav")
+
