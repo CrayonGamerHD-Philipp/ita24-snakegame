cf2b386215d540914360c38661b375653532deb6
khaled42 <khalednabea2@gmail.com>
2025-06-02 08:24:08 +0200

    Update pause_menu

--- effekte/effekte.py
+++ effekte/effekte.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.mixer.init()
 
-eat_sound = pygame.mixer.Sound("Sounds/eat.wav")
-background_music = pygame.mixer.Sound("Sounds/background_music.wav")
-game_over_sound = pygame.mixer.Sound("Sounds/game_over.wav")
+eat_sound = pygame.mixer.Sound("sounds/eat.wav")
+background_music = pygame.mixer.Sound("sounds/background_music.wav")
+game_over_sound = pygame.mixer.Sound("sounds/game_over.wav")
 
